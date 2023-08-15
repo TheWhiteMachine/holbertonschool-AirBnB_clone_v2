14d3683347c8ce76384a89899fc59e28dbf3db24
root <priveiro@gmail.com>
2023-08-15 09:51:07 -0700

    7 f

--- models/state.py
+++ models/state.py
@@ -13,17 +13,16 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), unique=True, nullable=False)
-        id = Column(String(60), primary_key=True, nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
     else:
-        # getter
-
+        @property
         def cities(self):
-            from engine.file_storage import FileStorage
-            from models.city import City
-            list_of_cities = []
-            for key in list(storage.all(City).values()):
-                if key.state_id == self.id:
-                    list_of_cities.append(key)
-            return list_of_cities
+            """Getter for FileStorage """
+            from models.city import City    # import here to avoid circular import
+            from models import storage
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
