3186e904fcf23aeb4002a292975605d206f46884
root <priveiro@gmail.com>
2023-08-15 09:42:28 -0700

    user wthout if

--- models/user.py
+++ models/user.py
@@ -4,20 +4,18 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "users"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128),  nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete, delete-orphan")
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128),  nullable=True)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
 
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                               backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
