b3353111026a678238329bdd21bdfd2e35adc055
root <priveiro@gmail.com>
2023-08-16 06:42:45 -0700

    task 8 fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """ this module start a web app with flask """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -10,12 +11,12 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ a web page with a list of state objects """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down():
+def tear_down(e):
     """ close the actual session """
     storage.close()
 
